b5475a5df7bc826246484adba61d6f259dcd3372
Srinivas706 <srinivasbodas706@gmail.com>
2025-06-21 21:11:39 -0500

    git add .

--- src/main/java/com/smartfit/SmartFitTracker/SmartFitTrackerApplication.java
+++ src/main/java/com/smartfit/SmartFitTracker/SmartFitTrackerApplication.java
@@ -2,8 +2,10 @@ package com.smartfit.SmartFitTracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.smartfit.SmartFitTracker.controller", "com.smartfit.SmartFitTracker.repository", "com.smartfit.SmartFitTracker.model"})
 public class SmartFitTrackerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SmartFitTrackerApplication.class, args);
