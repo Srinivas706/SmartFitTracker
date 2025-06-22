b1686d0c8857ed280c5262caf4ca9057278c9b00
Srinivas706 <srinivasbodas706@gmail.com>
2025-06-21 20:57:23 -0500

    git add .

--- src/main/java/com/smartfit/SmartFitTracker/controller/WorkoutController.java
+++ src/main/java/com/smartfit/SmartFitTracker/controller/WorkoutController.java
@@ -3,11 +3,13 @@ package com.smartfit.SmartFitTracker.controller;
 import com.smartfit.SmartFitTracker.model.Workout;
 import com.smartfit.SmartFitTracker.repository.WorkoutRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
+@Controller
 @RequestMapping("/api/workouts")
 public class WorkoutController {
 
@@ -20,7 +22,9 @@ public class WorkoutController {
     }
 
     @GetMapping
-    public List<Workout> getAllWorkouts() {
-        return workoutRepository.findAll();
+    public String getAllWorkouts(Model model) {
+        List<Workout> workouts = workoutRepository.findAll();
+        model.addAttribute("workouts", workouts);
+        return "workouts";
     }
 }
